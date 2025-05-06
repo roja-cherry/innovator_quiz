08a9c6ce800322fd570e2ce13d919761fea44401
parvathy2000 <parvathyanand101@gmail.com>
2025-05-06 15:47:16 +0530

    usesatae changes

--- src/components/quiz-management/QuizManagementTable.jsx
+++ src/components/quiz-management/QuizManagementTable.jsx
@@ -2,7 +2,32 @@ import React, { useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
 const QuizManagementTable = () => {
-  const [data, setData] = useState([0, 1, 2, 5, 6, 6, 5, 4, 4, 3, 3]);
+  const [data, setData] = useState([
+    {
+      quizId: 1,
+      quizName: "ABC",
+      createdAt: "June 1",
+      duration: 10
+    },
+    {
+      quizId: 2,
+      quizName: "ABC",
+      createdAt: "June 1",
+      duration: 10
+    },
+    {
+      quizId: 3,
+      quizName: "ABC",
+      createdAt: "June 1",
+      duration: 10
+    },
+    {
+      quizId: 4,
+      quizName: "ABC",
+      createdAt: "June 1",
+      duration: 10
+    }
+  ]);
 
   return (
     <div className="container mt-4">
@@ -18,9 +43,9 @@ const QuizManagementTable = () => {
         <tbody>
           {data.map((quiz, index) => (
             <tr key={index}>
-              <td scope="row">Q1</td>
-              <td>Air edn</td>
-              <td>10m</td>
+              <td scope="row">{quiz.quizName}</td>
+              <td>{quiz.duration}</td>
+              <td>{quiz.createdAt}</td>
               <td>
                 <FaEdit style={{ cursor: "pointer", marginRight: "10px" }} />
                 <FaTrash className="ms-2" style={{ cursor: "pointer" }} />
