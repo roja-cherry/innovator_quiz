ae5b0e6d6def20fb19ddb0de416d4c73136af50f
SandhuBabu <sandhubabu704@gmail.com>
2025-05-06 12:23:21 +0530

    react icons added

--- src/pages/admin/QuizManagement.jsx
+++ src/pages/admin/QuizManagement.jsx
@@ -4,7 +4,7 @@ const QuizManagement = () => {
   return (
     <section className="container-fluid p-5">
       <h2>Quiz Management</h2>
-      <div className="quiz-form-container p-5 mt-4">
+      <div className="p-5 mt-4">
 
         // filters
 
