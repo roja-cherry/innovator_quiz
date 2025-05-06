24abe895ccd6f7363bd12e7ba6a80ab814cce3f6
SandhuBabu <sandhubabu704@gmail.com>
2025-05-06 12:14:03 +0530

    created page for quiz management

---
+++ src/pages/admin/QuizManagement.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const QuizManagement = () => {
+  return (
+    <section className="container-fluid p-5">
+      <h2>Edit Quiz</h2>
+      <div className="quiz-form-container p-5 mt-4">
+
+        // filters
+
+        // table
+      </div>
+    </section>
+  )
+}
+
+export default QuizManagement
