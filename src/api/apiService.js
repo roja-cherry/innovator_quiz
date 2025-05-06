789a243ecfbdd0e5065e5e8806d17f0a6e37bbd9
Jeena2001 <jjoy19847@gmail.com>
2025-05-06 11:27:53 +0530

    apiService.js  added

---
+++ src/api/apiService.js
@@ -0,0 +1,24 @@
+import { axiosInstance } from "./axiosInstance";
+
+export const createQuiz = (formData) => {
+  return axiosInstance.post("/api/admin/quiz/create", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      
+    },
+  });
+};
+
+export const editQuiz = (formData, quizId) => {
+  return axiosInstance.put(`/api/admin/quiz/${quizId}`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+
+    },
+  });
+};
+
+export const getQuiz = (quizId) => {
+  return axiosInstance.get(`/api/admin/quiz/${quizId}`);
+};
+
