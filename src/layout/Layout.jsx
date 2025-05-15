eb556101758b82d0e740b06871c39498964b6c9b
SandhuBabu <sandhubabu704@gmail.com>
2025-05-15 11:50:02 +0530

    sidebar added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/navbar/Navbar'
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import { AppSidebar } from "../components/navbar/AppSidebar";
 
 const Layout = () => {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  )
-}
+    <AppSidebar>
+      <main className="flex-1">
+        {/* <Navbar /> */}
+        <Outlet />
+      </main>
+    </AppSidebar>
+  );
+  return <></>;
+};
 
-export default Layout
+export default Layout;
