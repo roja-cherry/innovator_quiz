c74702b188b409f98dd6a438510b9e6141b9e61e
SandhuBabu <sandhubabu704@gmail.com>
2025-05-18 00:31:18 +0530

    added icon in sidebar

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,38 +1,50 @@
 import React from "react";
+import { LuCalendarClock } from "react-icons/lu";
+import { RiListCheck3 } from "react-icons/ri";
+import { RxDashboard } from "react-icons/rx";
 import { NavLink } from "react-router-dom";
 export const adminUrls = [
   {
     text: "Dashboard",
     path: "/",
+    icon: RxDashboard,
   },
   {
     text: "Quiz Management",
     path: "/admin/quiz-management",
+    icon: RiListCheck3
   },
   {
     text: "Schedule",
     path: "/admin/schedule",
+    icon: LuCalendarClock
   },
 ];
 
-export const Sidebar = ({onMenuClick = () => {}}) => {
+export const Sidebar = ({ onMenuClick = () => {} }) => {
   return (
     <>
       <ul className="nav flex-column mt-4">
-        {adminUrls.map((url) => (
-          <NavLink
-            key={url?.path}
-            to={url.path}
-            className={({ isActive }) =>
-              `nav-link d-block px-4 py-3 mt-1 text-nowrap rounded font-medium text-decoration-none ${
-                isActive ? "bg-primary text-white" : "text-dark"
-              }`
-            }
-            onClick={onMenuClick}
-          >
-            {url.text}
-          </NavLink>
-        ))}
+        {adminUrls.map((url) => {
+          const Icon = url.icon
+          return (
+            <NavLink
+              key={url?.path}
+              to={url.path}
+              className={({ isActive }) =>
+                `nav-link fw-normal d-block px-4 py-3 mt-1 text-nowrap rounded font-medium text-decoration-none ${
+                  isActive
+                    ? "bg-primary_ text-primary text-white_"
+                    : "text-dark"
+                }`
+              }
+              onClick={onMenuClick}
+            >
+              <span className="me-3">{url?.icon && <Icon style={{fontSize: "1.5rem"}} />}</span>
+              <span>{url.text}</span>
+            </NavLink>
+          );
+        })}
       </ul>
     </>
   );
