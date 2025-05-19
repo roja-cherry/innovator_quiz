387b40f0a1a7f7cd1bc546be98f1f24a81816822
SandhuBabu <sandhubabu704@gmail.com>
2025-05-19 15:24:55 +0530

    user layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,40 @@ import { QUIZ_ATTEND_URL } from "./utilities";
 import { EditPublish } from "./pages/admin/publish/EditPublish";
 import { AttendQuiz } from "./pages/user/AttendQuiz";
 import { AdminLayout } from "./layout/AdminLayout";
+import { UserLayout } from "./layout/UserLayout";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Toaster />
       <Routes>
+        {/* admin routes here... */}
         <Route path="/" element={<AdminLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="admin/quiz-management" element={<QuizManagement />} />
-          <Route path="/admin/quiz-management/create-quiz" element={<CreateQuiz />} />
-          <Route path="/admin/quiz-management/edit/:id" element={<EditQuiz />} />
+          <Route
+            path="/admin/quiz-management/create-quiz"
+            element={<CreateQuiz />}
+          />
+          <Route
+            path="/admin/quiz-management/edit/:id"
+            element={<EditQuiz />}
+          />
           <Route path="/admin/schedule" element={<Publish />} />
-          <Route path="/admin/schedule/:id/re-schedule" element={<EditPublish />} />
-          <Route path="/admin/quiz-management/quiz/:id" element={<ViewQuiz />} /> 
+          <Route
+            path="/admin/schedule/:id/re-schedule"
+            element={<EditPublish />}
+          />
+          <Route
+            path="/admin/quiz-management/quiz/:id"
+            element={<ViewQuiz />}
+          />
         </Route>
-        <Route path={QUIZ_ATTEND_URL} element={<AttendQuiz />} />
+
+        {/* user routes here... */}
+        <Route element={<UserLayout />}>
+          <Route index path={QUIZ_ATTEND_URL} element={<AttendQuiz />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
