c619ac1a40b18cacea5773c403f54b5b3c79d50c
Jeena2001 <jjoy19847@gmail.com>
2025-05-20 15:43:22 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ export const App = () => {
           
           <Route index path={QUIZ_LOGIN_URL} element={<QuizLogin />} />
           <Route path="/start/:quizId" element={<StartQuiz />} />
-          <Route path="/attend/:quizId" element={<TakeQuiz />} />
+          <Route path="/attend-quiz/:quizId" element={<TakeQuiz />} />
 
         </Route>
 
